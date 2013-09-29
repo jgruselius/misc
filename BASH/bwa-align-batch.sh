d50b83c262da1127db8482ba94795049464e3417
Joel Gruselius <joel.gruselius@scilifelab.se>
2013-09-29 16:37:25 +0200

    Create bwa-align-batch.sh
    Script for running BWA alignment (on UPPMAX) for a bunch of files.

---
+++ BASH/bwa-align-batch.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -l
+
+#SBATCH -A b2013064
+#SBATCH -p node
+#SBATCH -n 1
+#SBATCH -e bwa-align-batch-%j.err
+#SBATCH -o bwa-align-batch-%j.out
+#SBATCH --mail-user joel.gruselius@scilifelab.se
+#SBATCH --mail-type=ALL
+
+# This script runs BWA aln on the specified files sequentially
+# Usage: bwa-align-batch [file(s)...]
+
+# Change to getopts?
+# Output path:
+DATA_DIR=/bubo/home/h28/joelg/b2013064/nobackup/joel
+# Path to genome reference:
+REF=/bubo/nobackup/uppnex/reference/biodata/genomes/Hsapiens/hg19/bwa/hg19.fa
+
+module load bioinfo-tools
+#module load samtools 
+module load bwa
+
+#time samtools faidx $REF
+#time bwa index $REF
+
+# Print all file paths for informations sake:
+sep="\n"
+echo -e "REFERENCE FILE:\n$REF"
+echo -e "OUTPUT DIR:\n$DATA_DIR/aln/"
+echo "FILES:"
+for file in "$@";do echo "$(basename $file)";done
+echo -e $sep
+
+# Run for each file specified:
+files="$@"
+for file in ${files[@]}
+do
+	echo -e "INPUT:\n$(readlink $file)"
+	echo -e "OUTPUT:\n$DATA_DIR/aln/$(basename ${file%fastq.gz})sai"
+	time bwa aln -t 8 $REF $(readlink $file) > $DATA_DIR/aln/$(basename ${file%fastq.gz})sai
+	echo -e $sep
+done
