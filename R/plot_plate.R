ca0287664eed425bbf6edd5bad5e06a5b991302c
Joel Gruselius <joel.gruselius@scilifelab.se>
2013-11-27 11:03:49 +0100

    Function now parses row and column from well label instead of requiring these as separate columns

--- R/plot_plate.R
+++ R/plot_plate.R
@@ -1,15 +1,13 @@
-
 plotPlate <- function(fileName) {
-    require(ggplot2)
-    if(!file.exists(fileName)) {
-        stop(paste("file:",fileName,"does not exist"))
-    }
-    data <- read.csv(file=fileName,head=TRUE,stringsAsFactors=FALSE)
-
-    ggplot(data[data$Volume==1,],aes(x=Column,y=Row)) +
-        geom_tile(aes(fill=MeasAvg)) +
-        scale_x_continuous(breaks=1:12) +
-        scale_y_discrete(limits=rev(LETTERS[1:8])) +
-        scale_fill_gradient(low="white",high="steelblue") +
-        facet_grid(~Volume)
+	require(ggplot2)
+	if(!file.exists(fileName)) {
+		stop(paste("file:",fileName,"does not exist"))
+	}
+	data <- read.csv(file=fileName,head=TRUE,stringsAsFactors=FALSE)
+	ggplot(data,aes(x=as.numeric(substring(Well.positions,2)),y=substring(Well.positions,1,1))) +
+		geom_tile(aes(fill=MeasAvg)) +
+		scale_x_continuous("Column",breaks=1:12) +
+		scale_y_discrete("Row",limits=rev(LETTERS[1:8])) +
+		scale_fill_gradient(low="white",high="steelblue") +
+		facet_grid(~Volume)
 }
