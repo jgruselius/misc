2dcfcb97c35bfce9ea5cb251cd2d445b271fb345
Joel Gruselius <joel.gruselius@scilifelab.se>
2013-12-05 16:33:08 +0100

    Update print-hs-metrics.sh

--- BASH/print-hs-metrics.sh
+++ BASH/print-hs-metrics.sh
@@ -6,17 +6,17 @@ echo -e "SAMPLE_ID,PROJECT,"$header
 # For each sample:
 for folder in $(echo data/*/)
 do
-  	# If the sample has been run on more than 1 FC:
-        if [ -d $folder"TOTAL" ]; then
-                spath=$folder"TOTAL"
-        else
-            	spath=$folder"*/"
-        fi
+	# If the sample has been run on more than 1 FC:
+	if [ -d $folder"TOTAL" ]; then
+		spath=$folder"TOTAL"
+	else
+		spath=$folder"*/"
+	fi
 	for file in $(find $spath -mindepth 1 -maxdepth 1 -name *.hs_metrics);
-        do
-          	project=$(basename $PWD)
-                sample=$(basename $folder)
-                metrics=$(cat $file | head -q -n 8 | tail -n 1 | tr "," "." | tr "\t" $
-                echo -e $sample","$project","$metrics
-        done
+	do
+		project=$(basename $PWD)
+		sample=$(basename $folder)
+		metrics=$(cat $file | head -q -n 8 | tail -n 1 | tr "," "." | tr "\t" $
+		echo -e $sample","$project","$metrics
+	done
 done
