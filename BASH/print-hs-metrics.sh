6426d79b749e6c3c5631008237218e7a5fd1eb25
Joel Gruselius <joel.gruselius@scilifelab.se>
2013-12-05 16:28:35 +0100

    Use single file for header

--- BASH/print-hs-metrics.sh
+++ BASH/print-hs-metrics.sh
@@ -1,21 +1,22 @@
 # Print header row:
-header=$(cat data/*/*/*.hs_metrics | head -q -n 7 | tail -n 1 | tr "\t" ",")
+firstfile=$(ls -d data/*/*/*hs_metrics | head -1)
+header=$(cat $firstfile | head -q -n 7 | tail -n 1 | tr "\t" ",")
 echo -e "SAMPLE_ID,PROJECT,"$header
 
 # For each sample:
 for folder in $(echo data/*/)
 do
-	# If the sample has been run on more than 1 FC:
-	if [ -d $folder"TOTAL" ]; then
-		spath=$folder"TOTAL"
-	else
-		spath=$folder"*/"
-	fi
-	for file in $(find $spath -mindepth 1 -maxdepth 1 -name *.hs_metrics)
-	do
-		project=$(basename $PWD)
-		sample=$(basename $folder)
-		metrics=$(cat $file | head -q -n 8 | tail -n 1 | tr "," "." | tr "\t" ",")
-		echo -e $sample","$project","$metrics
-	done
+  	# If the sample has been run on more than 1 FC:
+        if [ -d $folder"TOTAL" ]; then
+                spath=$folder"TOTAL"
+        else
+            	spath=$folder"*/"
+        fi
+	for file in $(find $spath -mindepth 1 -maxdepth 1 -name *.hs_metrics);
+        do
+          	project=$(basename $PWD)
+                sample=$(basename $folder)
+                metrics=$(cat $file | head -q -n 8 | tail -n 1 | tr "," "." | tr "\t" $
+                echo -e $sample","$project","$metrics
+        done
 done
