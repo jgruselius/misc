3a3bc9896b372f8059f08921f1b15f0292c61869
Joel Gruselius <joel.gruselius@scilifelab.se>
2014-01-21 17:14:57 +0100

    Update statusdb-index-read-view.js
    See comments in code for usage

--- JavaScript/statusdb-index-read-view.js
+++ JavaScript/statusdb-index-read-view.js
@@ -1,3 +1,13 @@
+/*
+To use this function as a CouchDB view, create a map.json file containing:
+	{"map":"function(doc) { ... }"}
+Remove all newline characters and make sure to use correct quoting.
+The StatusDB view can then be queried by running:
+	curl -s -H "Content-Type: application/json" --data "@map.json" \
+	http://USER:PASS@tools-dev.scilifelab.se:5984/flowcells/_temp_view
+Where USER and PASS should be replaced with the actual login details.
+*/
+
 function(doc) {
 	var d = new Date(doc['modification_time']);
 	var bcodes = doc['illumina']['Demultiplex_Stats']['Barcode_lane_statistics'];
