4392e0c914334de77f9b9c4f7d7361f20987cacb
Joel Gruselius <joel.gruselius@scilifelab.se>
2014-01-23 17:57:56 +0100

    Rewrote iteration of JSON data
    Restructured code and changed to use stdin/stdout instead of files

--- Python/statusdb-data-parser.py
+++ Python/statusdb-data-parser.py
@@ -1,32 +1,42 @@
+import sys
 import json
 import csv
+import time
 
-file="index-lane-yields.json"
-out_file="out.csv"
+def parse(in_file, out_file):
+	# Data object is of format:
+	# 	Total flowcells:	data["total_rows"]
+	# 	Offset: 			data["offset"]
+	# 	Flowcell data:		data["rows"]
+	# Where data["rows"] is a list and if fc = data["rows"][n]:
+	# 	Database hash key:	fc["id"]
+	#	Flowcell/run ID:	fc["key"]
+	#	Lane data:			fc["value"]
+	# Where fc["value"] is a list and if n is the lane index then fc["value"][n-1]
+	# is of format {barcode_sequence : million_reads}
 
-with open(file) as raw_data:
-	data = json.load(raw_data)
+	data = json.load(in_file)
 
-# Data object is of format:
-# 	Total flowcells:	data["total_rows"]
-# 	Offset: 			data["offset"]
-# 	Flowcell data:		data["rows"]
-# Where data["rows"] is a list and if fc = data["rows"][n]:
-# 	Database hash key:	fc["id"]
-#	Flowcell/run ID:	fc["key"]
-#	Lane data:			fc["value"]
-# Where fc["value"] is a list and if n is the lane index then fc["value"][n-1]
-# is of format {barcode_sequence : million_reads}
+	# Reformat to RUN-ID,LANE,BARCODE,YIELD
 
-# Reformat to RUN-ID,LANE,BARCODE,YIELD
-formd = []
-for fc in data["rows"]:
-	fc_id = fc["key"]
-	for lane_index, lane in enumerate(fc["value"]):
-		formd.append([[fc_id, lane_index, bc, m_reads] for bc, m_reads in lane.iteritems()])
+	# Write comma-separated output:
+	writer = csv.writer(out_file, delimiter=",")
 
-with open(out_file, "wb") as csv_file:
-	writer = csv.writer(csv_file, delimiter=",")
-	writer.writerow(["RUN-ID","LANE","BARCODE","M_READS"])
-	for rows in formd:
-		writer.writerows(rows)
+	# As an inappropriately long comprenhension:
+	writer.writerows([[fc["key"], i+1, bc, reads] \
+		for fc in data["rows"] \
+		for i, lane in enumerate(fc["value"]) \
+		for bc, reads in lane.iteritems()]
+	)
+
+	# As a nested loop:
+	# for fc in data["rows"]:
+	# 	for i, lane in enumerate(fc["value"]):
+	# 		for bc, reads in lane.iteritems():
+	# 			writer.writerow([fc["key"], i+1, bc, reads])
+
+if __name__ == "__main__":
+	t1 = time.clock()
+	parse(sys.stdin, sys.stdout)
+	print(time.clock() - t1)
+	
