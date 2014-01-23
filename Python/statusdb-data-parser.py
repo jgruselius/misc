3c79b558921cea8d2f52a67421945bccb37d2cec
Joel Gruselius <joel.gruselius@scilifelab.se>
2014-01-23 18:01:15 +0100

    Cleaned up code

--- Python/statusdb-data-parser.py
+++ Python/statusdb-data-parser.py
@@ -5,20 +5,18 @@ import csv
 def parse(in_file, out_file):
 	# Data object is of format:
 	# 	Total flowcells:	data["total_rows"]
-	# 	Offset: 			data["offset"]
+	# 	Offset: 		data["offset"]
 	# 	Flowcell data:		data["rows"]
 	# Where data["rows"] is a list and if fc = data["rows"][n]:
 	# 	Database hash key:	fc["id"]
 	#	Flowcell/run ID:	fc["key"]
-	#	Lane data:			fc["value"]
+	#	Lane data:		fc["value"]
 	# Where fc["value"] is a list and if n is the lane index then fc["value"][n-1]
 	# is of format {barcode_sequence : million_reads}
 
 	data = json.load(in_file)
 
-	# Reformat to RUN-ID,LANE,BARCODE,YIELD
-
-	# Write comma-separated output:
+	# Write comma-separated output with format RUN-ID,LANE,BARCODE,YIELD:
 	writer = csv.writer(out_file, delimiter=",")
 
 	# As an inappropriately long comprenhension:
@@ -27,12 +25,6 @@ def parse(in_file, out_file):
 		for i, lane in enumerate(fc["value"]) \
 		for bc, reads in lane.iteritems()]
 	)
-
-	# As a nested loop:
-	# for fc in data["rows"]:
-	# 	for i, lane in enumerate(fc["value"]):
-	# 		for bc, reads in lane.iteritems():
-	# 			writer.writerow([fc["key"], i+1, bc, reads])
 
 if __name__ == "__main__":
 	parse(sys.stdin, sys.stdout)
