2054cddd9256f3b35e323f9d23a846e12464cc56
Joel Gruselius <joel.gruselius@scilifelab.se>
2014-01-25 19:49:37 +0100

    Corrected number parsing

--- JavaScript/statusdb-index-read-view.js
+++ JavaScript/statusdb-index-read-view.js
@@ -11,11 +11,11 @@ Where USER and PASS should be replaced with the actual login details.
 function(doc) {
 	var d = new Date(doc['modification_time']);
 	var bcodes = doc['illumina']['Demultiplex_Stats']['Barcode_lane_statistics'];
-	if(d.getFullYear()==2013 && bcodes.length) {
+	if(d.getFullYear()>2012 && bcodes.length) {
 		var yields = {};
 		for(var bc in bcodes) {
 			var lane = parseInt(bcodes[bc]['Lane'],10);
-			var yield = bcodes[bc]['# Reads'].replace(',','');
+			var yield = bcodes[bc]['# Reads'].replace(/,/g,'');
 			if(!(lane in yields)) {
 				yields[lane] = {};
 			}
