f966454868121322c56007f022ee2301965521b0
Joel Gruselius <joel.gruselius@scilifelab.se>
2014-01-28 17:35:22 +0100

    Updated stats_by_category.R

--- R/stats_by_category.R
+++ R/stats_by_category.R
@@ -1,23 +1,26 @@
 stats_by_category <- function(df, cat, group_by1, group_by2) {
-	d <- data.frame()
+	# Create an empty data frame with column names:
+	headings <- c("min","max","mean","stdev","cv")
+	headings <- c(group_by1,group_by2,sapply(h,paste,cat,sep="."))
+	# Subset data:
 	sub <- unique(df[,group_by1])
 	for(i in sub) {
 		sub <- unique(df[df[group_by1]==i,group_by2])
 		for(j in sub) {
-			vals <- df[df[group_by1]==i & df[group_by2]==j,]
-			avg <- mean(vals[!is.na(vals[cat]),cat])
-			sdev <- sd(vals[!is.na(vals[cat]),cat])
-			cv <- sdev/avg
-			h1 <- paste(cat,"mean",sep=".")
-			h2 <- paste(cat,"stdev",sep=".")
-			h3 <- paste(cat,"cv",sep=".")
-			d <- rbind(d,data.frame(group_by1=i,group_by2=j,h1=avg,h2=sdev,h3=cv))
+			vals <- df[df[group_by1]==i & df[group_by2]==j & !is.na(df[cat]),cat]
+			minv <- min(vals)
+			maxv <- max(vals)
+			avg <- mean(vals)
+			sdev <- ifelse(minv == maxv, NA, sd(vals))
+			cv <- ifelse(avg == 0, NA, sdev/avg)
+			newdf <- setNames(c(i,j,minv,maxv,avg,sdev,cv),headings)
+			if(exists("d")) {
+				d <- rbind(d,newdf)
+			} else {
+				d <- newdf
+			}
 		}
 	}
-	names(d)[1] <- group_by1
-	names(d)[2] <- group_by2
-	names(d)[3] <- h1
-	names(d)[4] <- h2
-	names(d)[5] <- h3
-	return(d)
+	rownames(d) <- NULL
+	return(as.data.frame(d))
 }
