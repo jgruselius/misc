a5fbc744ea1d685ff8f25d7ba0e1444db485a0aa
Joel Gruselius <joel.gruselius@scilifelab.se>
2014-03-31 17:40:14 +0200

    plotPlate better deals with non-numeric and NA values

--- R/plot_plate.R
+++ R/plot_plate.R
@@ -3,11 +3,12 @@ plotPlate <- function(fileName, val, plateCoord="Well", splitBy=NULL) {
 	if(!file.exists(fileName)) {
 		stop(paste("file:",fileName,"does not exist"))
 	}
-	dat <- read.csv(file=fileName,head=TRUE,stringsAsFactors=FALSE)
+	dat <- read.csv(file=fileName,head=TRUE,as.is=TRUE,na.strings=c("NA","N/A"))
 	dat$plateRow <- substring(dat[[plateCoord]],1,1)
 	dat$plateCol <- as.numeric(substring(dat[[plateCoord]],2))
+	dat$numVals <- as.numeric(dat[,val])
 	platePlot <- ggplot(data=dat,aes(x=plateCol,y=plateRow)) +
-		geom_tile(aes_string(fill=val)) +
+		geom_tile(aes(fill=numVals,na.rm=TRUE)) +
 		scale_x_continuous("Column",breaks=1:12) +
 		scale_y_discrete("Row",limits=rev(LETTERS[1:8])) +
 		scale_fill_gradient(low="white",high="steelblue")
