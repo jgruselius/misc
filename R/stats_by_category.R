4006c3625a964e7ddc47f1ac85cd21b1064c8819
Joel Gruselius <joel.gruselius@scilifelab.se>
2014-05-12 10:06:53 +0200

    Improved handling of missing values
    - Added na.rm parameter (default=FALSE) to only use rows where the
    treated data is complete
    - Added count (occurrences) column

--- R/stats_by_category.R
+++ R/stats_by_category.R
@@ -1,19 +1,24 @@
 # Joel Gruselius 2014
 # Calculating some stats for variable 'categ' grouped by 'group1' and 'group2'
-stats_by_category <- function(df, categ, group1, group2) {
+# Set na.rm=TRUE to only use complete rows of df (i.e. rows with no missing 
+# value in any column)
+stats_by_category <- function(df, categ, group1, group2=NULL, na.rm=FALSE) {
 	require(plyr)
-		sfun <- function(x, col) {
-			c(min = min(x[[col]]),
-			max = max(x[[col]]),
-			mean = mean(x[[col]]),
-			median = median(x[[col]]),
-			sum = sum(x[[col]]),
-			stdev = sd(x[[col]]),
-			cv = sd(x[[col]]) / mean(x[[col]]))
-		}
+	sfun <- function(x, col) {
+		num <- as.numeric(x[[col]])
+		c(count  = length(num),
+		  min    = min(num),
+		  max    = max(num),
+		  mean   = mean(num),
+		  median = median(num),
+		  sum    = sum(num),
+		  stdev  = sd(num),
+		  cv     = sd(num) / mean(num))
+	}
+	if(na.rm) df <- df[complete.cases(df[,c(categ, group1, group2)]),]
 	summary <- ddply(df, c(group1, group2), .fun=sfun, categ)
 	header <- c("min", "max", "mean", "median", "sum", "stdev", "cv")
-	header <- c(group1, group2, sapply(header, paste, categ, sep="."))
+	header <- c(group1, group2, "count", sapply(header, paste, categ, sep="."))
 	colnames(summary) <- header
 	return(summary)
 }
