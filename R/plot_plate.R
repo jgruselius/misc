f6d3965fe3f79604a4c277f2e12ca87fba4bb5dc
Joel Gruselius <joel.gruselius@scilifelab.se>
2015-01-26 15:45:58 +0100

    Added numeric values to plate plot

--- R/plot_plate.R
+++ R/plot_plate.R
@@ -11,7 +11,8 @@ plotPlate <- function(fileName, val, plateCoord="Well", splitBy=NULL) {
 		geom_tile(aes(fill=numVals,na.rm=TRUE)) +
 		scale_x_continuous("Column",breaks=1:12) +
 		scale_y_discrete("Row",limits=rev(LETTERS[1:8])) +
-		scale_fill_gradient(low="white",high="steelblue")
+		scale_fill_gradient(low="white",high="steelblue",name=val) +
+		geom_text(aes(label=signif(numVals,3)),size=2,col="#666666")
 	if(!is.null(splitBy)) {
 		platePlot <- platePlot + facet_grid(paste("~",splitBy,sep=""))
 	}
