d1226ad5e610366a20fda0d20e140a1f7dfcc127
Joel Gruselius <joel.gruselius@scilifelab.se>
2015-07-03 16:07:10 +0200

    Added reference to root, removed obsolete file

--- Python/duplicate-finder.py
+++ Python/duplicate-finder.py
@@ -9,9 +9,10 @@ import time
 
 # Traverses the directory tree and returns a dict mapping all file names found
 # more than once to a list of path-hash pairs:
-def traverse(top):
+def traverse(top, max_depth=None):
 	fnames = {}
-	fhash = {}
+	fhash = {"__root__": top}
+	i = 0
 	for root, dirs, files in os.walk(top, topdown=False):
 		for file in files:
 			path = os.path.join(root, file)
@@ -28,12 +29,18 @@ def traverse(top):
 			else:
 				# Temporarily store path as well if filename should be encountered again:
 				fnames[file] = path
+		i += 1
+		# Stop if we have descended deeper than max_depth in the directory tree:
+		if max_depth != None and i >= max_depth:
+			break
 	return fhash
 
 # Print paths for each filename grouped by hash (in color!)
-def print_dupes(top):
-	file_dict = traverse(top)
-	for file, fdict in file_dict.items():
+def print_dupes(dup_obj):
+	top = dup_obj["__root__"]
+	for file, fdict in dup_obj.items():
+		if file is "__root__":
+			continue
 		print("{}{}{} was encountered {!s} times:".format(Fore.BLUE, file,
 			Fore.RESET, len(fdict)))
 		# Create a list of filepaths for each unique hash:
@@ -54,12 +61,15 @@ def print_dupes(top):
 		print("\n")
 
 # Print paths for each filename (ungrouped, no color)
-def print_dupes_unsorted(top):
-	file_dict = traverse(top)
-	for file, fdict in file_dict.items():
+def print_dupes_unsorted(dup_obj):
+	top = dup_obj["__root__"]
+	for file, fdict in dup_obj.items():
+		if file is "__root__":
+			continue
 		print("{0} was encountered {1!s} times:".format(file, len(fdict)))
 		for path, hash in fdict.items():
-			print("{0}/\n{1}".format(os.path.split(os.path.relpath(path, start=top))[0], hash[:8]))
+			print("{0}/\n{1}".format(os.path.split(os.path.relpath(path,
+				start=top))[0], hash[:8]))
 		print("\n")
 
 def hashfile(path, hasher, blocksize=65536):
@@ -71,13 +81,14 @@ def hashfile(path, hasher, blocksize=65536):
 	return hasher.hexdigest()
 
 def benchmark(path, n=99):
+	dup_obj = traverse(path)
 	temp = sys.stdout
 	sys.stdout = None
 	res = []
 	for f in (print_dupes, print_dupes_unsorted):
 		t0 = time.clock()
 		for i in range(n):
-			f(path)
+			f(dup_obj)
 		t1 = time.clock() - t0
 		res.append("{}: {:.3f}s ({:.5f}s per iteration)\n".format(f.__name__, t1, t1/n))
 	sys.stdout = temp
@@ -86,7 +97,7 @@ def benchmark(path, n=99):
 def main(args):
 	path = os.path.abspath(os.path.expanduser(os.path.normpath(args[0])))
 	if os.path.exists(path):
-		print_dupes(path)
+		print_dupes(traverse(path))
 	else:
 		print("The directory \"{}\" does not exist".format(path))
 
