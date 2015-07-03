663ac6bdf08cdcb48f501c50516e57d69739a4db
Joel Gruselius <joel.gruselius@scilifelab.se>
2015-07-03 16:03:45 +0200

    Quoting variables..

--- BASH/print-hs-metrics.sh
+++ BASH/print-hs-metrics.sh
@@ -1,22 +1,33 @@
+#!/bin/bash
+
 # Print header row:
-firstfile=$(ls -d data/*/*/*hs_metrics | head -1)
-header=$(cat $firstfile | head -q -n 7 | tail -n 1 | tr "\t" ",")
-echo -e "SAMPLE_ID,PROJECT,"$header
+firstfile=$(find . -name "*.hs_metrics" | head -1)
+header=$(head -q -n 7 "$firstfile" | tail -n 1 | tr "\t" ",")
+echo -e "SAMPLE_ID,PROJECT,""$header"
+
+if [ -d "data/" ]; then
+		spath="data/*/"
+	else
+		spath="./*/"
+	fi
+
+# Fix:
+spath="./*/"
 
 # For each sample:
-for folder in $(echo data/*/)
+for folder in $(echo "$spath")
 do
 	# If the sample has been run on more than 1 FC:
-	if [ -d $folder"TOTAL" ]; then
+	if [ -d "$folder""TOTAL" ]; then
 		spath=$folder"TOTAL"
 	else
 		spath=$folder"*/"
 	fi
-	for file in $(find $spath -mindepth 1 -maxdepth 1 -name *.hs_metrics);
+	for file in "$spath""*.hs_metrics";
 	do
-		project=$(basename $PWD)
-		sample=$(basename $folder)
-		metrics=$(cat $file | head -q -n 8 | tail -n 1 | tr "," "." | tr "\t" $
-		echo -e $sample","$project","$metrics
+		project=$(basename "$PWD")
+		sample=$(basename "$folder")
+		metrics=$(head -q -n 8 "$file" | tail -n 1 | tr "," "." | tr "\t" ",")
+		echo -e "$sample,$project,$metrics"
 	done
 done
