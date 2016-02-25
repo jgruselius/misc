65d13af897b59d2a84f3d4d1cafac166c331585a
Joel Gruselius <joel.gruselius@scilifelab.se>
2016-02-25 15:42:36 +0100

    Minor improvements to statusdb shell script

--- BASH/statusdb-get.sh
+++ BASH/statusdb-get.sh
@@ -1,17 +1,17 @@
 url="tools-dev.scilifelab.se:5984/flowcells/_temp_view"
 file=$1
-if [ -z "$file" ] || [ ! -f $file ] ; then 
-	echo "\nUsage:\n\t$0 <mapfunc.js>\n" >&2
+if [ -z "$file" ] || [ ! -f "$file" ] ; then
+	echo -e "\nUsage:\n\t$0 <mapfunc.js>\n" >&2
 	exit
 fi
 # Remove newlines and tabs:
-func=$(cat $file | tr "\n" " " | tr -d "\t")
+func=$(tr "\n" " " < "$file" | tr -d "\t")
 # Remove comments:
 func=$(echo "$func" | perl -pe "s/(\/\*)[^\*]+\*\///")
 json="{\"map\":\"$func\"}"
 echo "Using server URL: http://$url" >&2
-read -p "Username for server: " user
-read -s -p "Password for $user: " pass
-curl -s -H "Content-Type: application/json" --data "$json" http://$user:$pass@$url
+read -rp "Username for server: " user
+read -rs -p "Password for $user: " pass
+curl -s -H "Content-Type: application/json" --data "$json" http://"$user":"$pass"@"$url"
 unset user
 unset pass
