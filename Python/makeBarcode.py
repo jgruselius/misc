88cfe64aa5dbf2b520ab7b4b71ace2e512c463f4
Joel Gruselius <joel.gruselius@scilifelab.se>
2019-11-19 13:56:08 +0100

    Use scalable font for text-only labels

--- Python/makeBarcode.py
+++ Python/makeBarcode.py
@@ -1,23 +1,26 @@
 import argparse
 
 def makeBarcode(label, print_bc):
-    print("^XA") #start of label
-    print("^DFFORMAT^FS") #download and store format, name of format, end of field data (FS = field stop)
+    print("^XA") # start of label
+    print("^DFFORMAT^FS") # download and store format, name of format, end of field data (FS = field stop)
     print("^LH0,0") # label home position (label home = LH) 
     if print_bc:
-        print("^FO400,20^AFN,60,20^FN1^FS") #AF = assign font F, field number 1 (FN1), print text at position field origin (FO) rel. to home
-        print("^FO120,5^BCN,70,N,N^FN2^FS") #BC=barcode 128, field number 2, Normal orientation, height 70, no interpreation line.
+        print("^FO400,20^AFN,60,20^FN1^FS") # AF = assign font F, field number 1 (FN1), print text at position field origin (FO) rel. to home
+        print("^FO120,5^BCN,70,N,N^FN2^FS") # BC=barcode 128, field number 2, normal orientation, height 70, no interpretation line.
     else:
-        if len(label) > 21:
-            print("^FO20,20^AEN,60,20^FN1^FS") #AF = assign font F, field number 1 (FN1), print text at position field origin (FO) rel. to home
-        else:
-            print("^FO20,20^ADN,60,20^FN1^FS") #AF = assign font F, field number 1 (FN1), print text at position field origin (FO) rel. to home
-    print("^XZ") #end format
-    print("^XA") #start of label format
-    print("^XFFORMAT^FS") #label home posision
-    print("^FN1^FD{}^FS".format(label)) #this is readable
+        ch_size="32"
+        if len(label) > 42:
+            ch_size = 24
+        elif len(label) < 20:
+            ch_size = 40
+        # Scalable font ^A0N,32,32 should fit roughly 42 chars on our current labels
+        print("^FO20,40^A0N,{0},{0}^FB640,1,0,C,0^FN1^FS".format(ch_size)) # FO = x,y relative field origin; A0N = scalable font height,width; FB = make into one line field block and center
+    print("^XZ") # end format
+    print("^XA") # start of label format
+    print("^XFFORMAT^FS") # label home posision
+    print("^FN1^FD{}^FS".format(label)) # this is readable
     if print_bc:
-        print("^FN2^FD{}^FS".format(label)) #this is the barcode
+        print("^FN2^FD{}^FS".format(label)) # this is the barcode
     print("^XZ")
 
 def main(args):
